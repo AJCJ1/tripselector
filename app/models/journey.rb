0f4dfaf942a02b6a50078076701f9bf27bd651e4
Victor de Quercize <vdequercize@hotmail.com>
2023-01-10 11:58:14 +0100

    Capped the mew length of a journey to 21 days due to lack of cities to have longer journeys

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,8 +3,15 @@ class Journey < ApplicationRecord
   has_many :city_journeys, dependent: :destroy
   has_many :cities, through: :city_journeys
   validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :end_date, presence: true, comparison: { greater_than: :start_date }
   validates :name, presence: true
   validates :country, presence: true
   validates :tag, presence: true
+  validate :journey_length
+
+  def journey_length
+    if end_date - start_date > 22
+      errors.add(:end_date, "can't be more than 21 days after start date")
+    end
+  end
 end
